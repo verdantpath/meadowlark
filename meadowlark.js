b5082fa0b562deb38b483eda5455c62ebad7bc98
verdantpath <m_dittmann@yahoo.com>
2020-09-20 19:38:34 -0700

    Add Handlebars, new routes

--- meadowlark.js
+++ meadowlark.js
@@ -1,32 +1,46 @@
 const express = require('express')
+const expressHandlebars = require('express-handlebars')
+
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
 
 const app = express()
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.type('text/plain')
-  res.send('Meadowlark Travel')
+  res.render('home')
 })
 
 app.get('/about', (req, res) => {
-  res.type('text/plain')
-  res.send('About Meadowlark Travel')
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render('about', { fortune: randomFortune })
 })
 
 // custom 404 page
 app.use ((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press CTRl-C to terminate.`))
