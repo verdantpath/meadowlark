e46f67ad2d3feb430caafe06de5a3c0fcd71f353
verdantpath <m_dittmann@yahoo.com>
2020-10-05 21:50:48 -0700

    Add Jest for testing

--- meadowlark.js
+++ meadowlark.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
-const fortune = require('/lib/fortune.js')
+const handlers = require('./lib/handlers')
 
 const app = express()
 
@@ -15,25 +15,22 @@ app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.render('home')
-})
+app.get('/', handlers.home)
 
-app.get('/about', (req, res) => {
-  res.render('about', { fortune: fortune.getFortune() })
-})
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use ((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use (handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError)
 
-app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press CTRl-C to terminate.`))
+if(require.main === module) {
+  app.listen(port, () => {
+    console.log( `Express started on http://localhost:${port}` + '; press ctrl-C to terminate.' )
+  })
+} else {
+  module.exports = app
+}
+
+// app.listen(port, () => console.log(`Express started on http://localhost:${port}; ` + `press CTRl-C to terminate.`))
